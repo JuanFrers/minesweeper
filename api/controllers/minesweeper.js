c17459cc8993e3a2d0c6823a8d06288145fb569d
Juan Frers <juan.frers@nan-labs.com>
2017-07-02 20:35:59 -0300

    Api schema updated and integrated with game logic

--- api/controllers/minesweeper.js
+++ api/controllers/minesweeper.js
@@ -1,15 +1,39 @@
+const Minesweeper = require('../helpers/minesweeper-core');
+let minesweeper;
+
 function start(req, res) {
+  try {
     const gameOptions = req.swagger.params.gameOptions.value;
+    minesweeper = new Minesweeper(gameOptions.rows, gameOptions.columns, gameOptions.mines);
     res.json({success: true});
+  } catch(e) {
+    console.error(e);
+    res.json({success: false});
+  }
+
 }
 
 function reveal(req, res) {
-    const column = req.swagger.params.column.value;
-    const row = req.swagger.params.row.value;
-    res.json({});
+  const column = req.swagger.params.column.value;
+  const row = req.swagger.params.row.value;
+  const revealResult = minesweeper.reveal(column, row);
+  const result = {
+    revealResult,
+    gameFinished: minesweeper.gameFinished,
+    gameWon: minesweeper.gameWon
+  };
+  res.json(result);
+}
+
+function flag(req, res) {
+  const column = req.swagger.params.column.value;
+  const row = req.swagger.params.row.value;
+  const result = minesweeper.flag(column, row);
+  res.json(result);
 }
 
 module.exports = {
-    start,
-    reveal
+  start,
+  reveal,
+  flag
 };
